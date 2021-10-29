1a9ab3f085bfa01ceacd2bb4a5ba48f2e95557f1
M'hamed Bendenia <medbendenia27@gmail.com>
2021-10-29 14:23:30 +0100

    Add documentation

---
+++ README.md
@@ -0,0 +1,16 @@
+# Forum API
+
+A Forum REST API using Django Rest Framework
+
+## Installation
+
+### Requirements
+- Python
+- Django
+
+    requirement list available in requirements.txt file
+    just type pip install -r requirements.txt
+
+# RESTAPI Docs
+I have added `drf-yasg` for API documentation:
+Redoc docs:  http://127.0.0.1:8000/redoc/
--- forum/urls.py
+++ forum/urls.py
@@ -14,11 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
 # from forum_api import urls
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version="v1",
+        description="Blog API using Django rest framework",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("forum_api.urls", namespace="forum_api")),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
